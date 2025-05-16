3be0dbee4a2ade1745436e1635b774981c9cedc7
Sibtain Syed <91776856+Shah621@users.noreply.github.com>
2025-05-17 00:57:27 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-from transformers import pipeline
-print("Transformers imported successfully.")
+# from transformers import pipeline
+# print("Transformers imported successfully.")
+
+#Just a testing file to see if transformers package is installed and imported successfully.
